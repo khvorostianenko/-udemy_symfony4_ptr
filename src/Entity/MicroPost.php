bbd40d2b74c459fd9e400ef56cad0e832e168249
mkhl <mikhail.khvorostianenko@gmail.com>
2018-12-10 22:51:37 +0200

    lecture 26 Creating first Entity

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ * @ORM\Table()
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+}
