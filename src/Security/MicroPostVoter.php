9e3e5a6f9f84ca2c509455245a0f6b350787c2df
mkhl <mikhail.khvorostianenko@gmail.com>
2018-12-17 19:15:03 +0200

    lecture 51 Adding an Admin User with all permissions

--- src/Security/MicroPostVoter.php
+++ src/Security/MicroPostVoter.php
@@ -6,12 +6,23 @@ namespace App\Security;
 use App\Entity\MicroPost;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManager;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManagerInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class MicroPostVoter extends Voter 
 {
     const EDIT = 'edit';
     const DELETE = 'delete';
+    /**
+     * @var AccessDecisionManagerInterface
+     */
+    private $accessDecisionManager;
+    
+    public function __construct(AccessDecisionManagerInterface $accessDecisionManager)
+    {
+        $this->accessDecisionManager = $accessDecisionManager;
+    }
     
     /**
      * Determines if the attribute and subject are supported by this voter.
@@ -50,6 +61,10 @@ class MicroPostVoter extends Voter
      */
     protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
     {
+        if ($this->accessDecisionManager->decide($token, [User::ROLE_ADMIN])) {
+            return true;
+        }
+        
         $authenticatedUser = $token->getUser();
         
         if (!$authenticatedUser instanceof User) {
