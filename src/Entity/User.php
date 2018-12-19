768dd092b070308004a5a3bacb8a5fd34f78eee8
mkhl <mikhail.khvorostianenko@gmail.com>
2018-12-19 09:05:48 +0200

    lecture 56 ManyToMany self-referencing relation (following/followers)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,9 +68,30 @@ class User implements UserInterface, \Serializable
      */
     private $posts;
     
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="following")
+     */
+    private $followers;
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="followers")
+     * @ORM\JoinTable(name="following", 
+     *     joinColumns={
+     *         @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *     },
+     *     inverseJoinColumns={
+     *         @ORM\JoinColumn(name="following_user_id", referencedColumnName="id")
+     *     }
+     * )
+     */
+    private $following;
+    
     public function __construct()
     {
         $this->posts = new ArrayCollection();
+        $this->followers = new ArrayCollection();
+        $this->following = new ArrayCollection();
     }
     
     /**
@@ -254,5 +275,21 @@ class User implements UserInterface, \Serializable
     public function getPosts()
     {
         return $this->posts;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getFollowing()
+    {
+        return $this->following;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getFollowers()
+    {
+        return $this->followers;
     }
 }
